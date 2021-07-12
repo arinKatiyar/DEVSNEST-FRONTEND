29cf2c25dca39589f1a2d5e22b84805b37f405f9
Arin Katiyar <63258407+arinKatiyar@users.noreply.github.com>
2021-07-12 20:35:00 +0530

    Add files via upload

---
+++ DAY 17/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata=[
+    {
+        food:"Pizza",
+        calory:"56",
+    },
+
+    {
+        food:"Burger",
+        calory:"69",
+    }
+    ,
+    {
+        food:"Coke",
+        calory:"500",
+    },
+    {
+        food:"Brownie",
+        calory:"180",
+    },
+    {
+        food:"Fried Rice",
+        calory:"90",
+    },
+    {
+        food:"Lassania",
+        calory:"200",
+    },
+    {
+        food:"Pani Puri",
+        calory:"10",
+    },
+];
+
+export default Sdata;
